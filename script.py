08eadd9c2b051fe676e10ca862a9e44983da5efa
duyatfit <pham@fitanalytics.com>
2019-10-17 15:05:53 +0200

    remove commented out codes

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 from lib_one import lib_one_code
-# from libtwo import lib_two_code
+from libtwo import lib_two_code
 import apache_beam as beam
 
 
 def main():
     print(lib_one_code.lib_one_hello())
     print(lib_one_code.np_arange(10))
-    # print(lib_two_code.lib_two_hello(10))
+    print(lib_two_code.lib_two_hello(10))
 
 
 if __name__ == "__main__":
